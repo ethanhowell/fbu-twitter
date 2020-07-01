1914fccba81c8b9a39c3f6aa867138271d2433b0
Ethan Howell <21162595+ethanhowell@users.noreply.github.com>
2020-07-01 15:40:50 -0500

    mostly got the correct layout for the compose tweet

--- app/src/main/java/com/ethanjhowell/tweeter/activities/ComposeActivity.java
+++ app/src/main/java/com/ethanjhowell/tweeter/activities/ComposeActivity.java
@@ -1,12 +1,14 @@
 package com.ethanjhowell.tweeter.activities;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import com.ethanjhowell.tweeter.R;
 import com.ethanjhowell.tweeter.databinding.ActivityComposeBinding;
+import com.ethanjhowell.tweeter.databinding.ToolbarBinding;
+
+import java.util.Objects;
 
 public class ComposeActivity extends AppCompatActivity {
     private static final String TAG = ComposeActivity.class.getCanonicalName();
@@ -17,9 +19,13 @@ public class ComposeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityComposeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ToolbarBinding toolbarBinding = binding.include;
+        toolbarBinding.toolbar.setTitle("");
+        setSupportActionBar(toolbarBinding.toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+
+        EditText etTweetBody = binding.etTweetBody;
+        // show keyboard and focus the edittext
+        etTweetBody.requestFocus();
     }
 }
